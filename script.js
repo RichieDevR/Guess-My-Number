e0f8f09d9386a278bc899e1b5c604b6345e6f01f
richard rosario <richie.rosario@protonmail.com>
2022-01-17 19:50:51 -0500

    emojis and logic fix

--- script.js
+++ script.js
@@ -4,22 +4,23 @@
 
 const check = document.querySelector('.check');
 const again = document.querySelector('.again');
-let score = 20;
+let score = 0;
 let highScore = 0;
 const displayMessage = message => {
   document.querySelector('.message').textContent = message;
 };
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(secretNumber);
 
 check.addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
-    displayMessage('You need to guess a number bro!');
+    displayMessage('⛔️You need to guess a number bro!⛔️');
   } else if (guess === secretNumber) {
-    displayMessage("Bro,you guessed my number! You're an absolute chad!");
+    displayMessage("Bro,you guessed my number! 🤩You're an absolute chad!🤩");
     document.querySelector('.number').textContent = secretNumber;
+    score +=10
+    document.querySelector('.score').textContent = score;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
@@ -29,24 +30,23 @@ check.addEventListener('click', () => {
     }
   } else if (guess !== secretNumber) {
     if (score > 0) {
-      displayMessage(guess > secretNumber ? 'Too High Bro!' : 'Too Low Bro!');
+      displayMessage(guess > secretNumber ? 'Too High Bro!😦' : 'Too Low Bro!☹️');
       score -= 1;
       document.querySelector('.score').textContent = score;
     } else {
-      displayMessage(`Bro you suck, Game Over! My number was ${secretNumber}`);
+      displayMessage(`👎Bro you suck, Game Over! My number was ${secretNumber}`);
       document.querySelector('.score').textContent = 0;
     }
   }
 });
 
 again.addEventListener('click', () => {
-  score = 20
+  score = highScore;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(secretNumber);
-  displayMessage('Start guessing bro...')
+  displayMessage('Start guessing bro...');
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
-   document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
-})
+});
